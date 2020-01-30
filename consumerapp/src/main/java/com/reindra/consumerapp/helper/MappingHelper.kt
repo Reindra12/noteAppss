31de3dae182952e22f8def59ab068b9b694a95e6
Reindra12 <reindrairawan@gmail.com>
2020-01-31 01:49:07 +0700

    copas dari class noteApps

---
+++ consumerapp/src/main/java/com/reindra/consumerapp/helper/MappingHelper.kt
@@ -0,0 +1,30 @@
+package com.reindra.mynotesapp.helper
+
+import android.database.Cursor
+import com.reindra.mynotesapp.db.DatabaseContract
+import com.reindra.mynotesapp.entity.Note
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+
+        while (notesCursor.moveToNext()) {
+            val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            notesList.add(Note(id, title, description, date))
+        }
+
+        return notesList
+    }
+
+    fun mapCursorToObject(notesCursor: Cursor): Note {
+        notesCursor.moveToFirst()
+        val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+        val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+        val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+        val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+        return Note(id, title, description, date)
+    }
+}
