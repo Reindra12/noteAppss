57e17231b92c530b174ac5d66719702e22f710e2
Reindra12 <reindrairawan@gmail.com>
2020-01-31 01:14:56 +0700

    modif class dabasecontract

--- app/src/main/java/com/reindra/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/com/reindra/mynotesapp/db/DatabaseContract.kt
@@ -1,18 +1,23 @@
 package com.reindra.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
+import android.service.notification.Condition.SCHEME
 
-internal class DatabaseContract {
+object DatabaseContract {
 
-    internal class NoteColumns : BaseColumns {
     const val AUTHORITY = "com.reindra.mynotesapp"
     const val SCHEME = "content"
+
+
+     class NoteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "note"
             const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+
             // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
             val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
                 .authority(AUTHORITY)
