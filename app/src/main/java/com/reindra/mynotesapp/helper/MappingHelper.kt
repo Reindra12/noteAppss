63bb5bbf23163e98997fc7696c5c39705c0de0d0
Reindra12 <reindrairawan@gmail.com>
2020-01-31 01:29:51 +0700

    menambahkan fungsi tambahan pada kelas MappingHelper untuk konversi dari cursor menjadi object dengan nama mapCursorToObject yang akan digunakan di kelas NoteAddUpdateActivity nanti.

--- app/src/main/java/com/reindra/mynotesapp/helper/MappingHelper.kt
+++ app/src/main/java/com/reindra/mynotesapp/helper/MappingHelper.kt
@@ -4,10 +4,10 @@ import android.database.Cursor
 import com.reindra.mynotesapp.db.DatabaseContract
 import com.reindra.mynotesapp.entity.Note
 object MappingHelper {
-
+    
     fun mapCursorToArrayList(notesCursor: Cursor): ArrayList<Note> {
         val notesList = ArrayList<Note>()
-        notesCursor.moveToFirst()
+
         while (notesCursor.moveToNext()) {
             val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
             val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
@@ -17,5 +17,14 @@ object MappingHelper {
         }
 
         return notesList
+    }
+
+    fun mapCursorToObject(notesCursor: Cursor): Note {
+        notesCursor.moveToFirst()
+        val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+        val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+        val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+        val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+        return Note(id, title, description, date)
     }
 }
